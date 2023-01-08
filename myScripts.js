5e8c7c84a778a033e0175f59591b648209718b5f
dav-kar-projects <114919530+dav-kar-projects@users.noreply.github.com>
2023-01-08 20:28:09 +0100

    Anfrage1.html until child and age

--- myScripts.js
+++ myScripts.js
@@ -2,6 +2,16 @@ var heroImageHeight = 0;
 var headerHeight = 0;
 var sendingButton = 0;
 var submitButton = 0;
+var minusButton = 0;
+var plusButton = 0;
+var numberField = 0;
+var minusButtonC = 0;
+var plusButtonC = 0;
+var numberFieldC = 0;
+var childrenInput;
+var plusButtons;
+var minusButtons;
+
 function ready() {
    let element = document.getElementById('header');
    let headerImage = document.querySelector('.js_hero_idclass');
@@ -22,8 +32,92 @@ function ready() {
    submitButton = document.getElementById('absenden_btn');
    sendingButton = document.querySelector('.sending');
    console.log(submitButton);
+   /// Eltern 
+   minusButton = document.querySelector('.form-number__minus');
+   plusButton = document.querySelector('.form-number__plus');
+   numberField = document.querySelector('.form-number');
+   minusButtonC = document.querySelector('.form-number__minusC');
+   plusButtonC = document.querySelector('.form-number__plusC');
+   numberFieldC = document.querySelector('.contact__children');
+   childrenInput = document.querySelector('.contact__children');
+   plusButtons = document.querySelectorAll('.form-number__plus');
 
+   minusButtons = document.querySelectorAll('.form-number__minus');
+
+
+   if (plusButton) {
+
+      plusButtons.forEach(button => button.addEventListener('click', increaseValue));
+      minusButtons.forEach(button => button.addEventListener('click', decreaseValue));
+      console.log(plusButtons);
+
+   }
 }
+
+function showChildAges() {
+   // Get the value of the contact__children input
+   const numChildren = document.querySelector('.contact__children').value;
+   console.log(numChildren);
+   // Get all of the contact__child-age elements
+   const childAgeElements = document.querySelectorAll('.contact__child-age');
+   const mainPart = document.querySelectorAll('.contact__children-ages');
+   if (numChildren > 0) {
+      mainPart[0].style.display = 'inline';
+   }
+   else {
+      mainPart[0].style.display = 'none';
+   }
+   // Set the visibility of each child age element based on the value of the contact__children input
+   for (let i = 0; i < childAgeElements.length; i++) {
+      if (i < numChildren) {
+         childAgeElements[i].style.display = 'inline';
+      } else {
+         childAgeElements[i].style.display = 'none';
+      }
+   }
+}
+
+function increaseValue(event) {
+   // Get the input element that is associated with the button that was clicked
+   var inputElement = event.target.previousSibling.previousSibling;
+
+   // Get the current value of the input element
+   let currentValue = parseInt(inputElement.value);
+
+   // Increase the value by 1
+   currentValue += 1;
+
+   // Update the value of the input element
+   inputElement.value = currentValue;
+}
+
+function decreaseValue(event) {
+   // Get the input element that is associated with the button that was clicked
+   var inputElement = event.target.nextElementSibling;
+
+   // Get the current value of the input element
+   let currentValue = parseInt(inputElement.value);
+
+   // Decrease the value by 1, but not below the minimum value
+   currentValue = Math.max(currentValue - 1, inputElement.min);
+
+   // Update the value of the input element
+   inputElement.value = currentValue;
+}
+
+function Decrease() {
+   numberField.value = parseInt(numberField.value, 10) - 1;
+}
+function Increase() {
+   numberField.value = parseInt(numberField.value, 10) + 1;
+}
+function DecreaseC() {
+   numberFieldC.value = parseInt(numberFieldC.value, 10) - 1;
+}
+function IncreaseC() {
+   numberFieldC.value = parseInt(numberFieldC.value, 10) + 1;
+}
+
 
 document.addEventListener("DOMContentLoaded", ready);
 
@@ -68,5 +162,4 @@ function DropdownFctApp() {
 //////////Anfrage senden /////////////
 // Get the two buttons
 
-// Add a click event listener to the submit button
 
